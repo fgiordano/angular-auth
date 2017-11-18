d9435f68a90a66d6049366ba721f6237507e9885
Francesca Giordano <francesca020406@hotmail.com>
2017-11-18 16:12:25 -0500

    adds the passport post routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user-model');
 
 const router = express.Router();
+
+router.post('/signup', (req, res, next) => {
+	const username = req.body.username;
+	const password = req.body.password;
+});
 
 
 module.exports = router;
