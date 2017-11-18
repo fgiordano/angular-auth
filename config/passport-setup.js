e30c978056910044afa6585b5688996f8f8e4a94
Francesca Giordano <francesca020406@hotmail.com>
2017-11-18 15:41:47 -0500

    sets up passport and session

---
+++ config/passport-setup.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+const User = require('../models/user-model');
+
+//serialize: save only the Id of the user document in the session
+passport.serializeUser(() => {
+	cb(null, loggedInUser._id);
+
+});
+
+// deserializa: retrive the full user details from the database using the Id 
+//(the user i stored in the session)
+passport.deserializeUser(() => {
+	User.findById(userIdFromSession, (err, userDocument) => {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		cb(null, userDocument);
+	})
+
+});
---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema(
+{
+	username: {
+		type: String,
+		required: [true, "Username is required"]
+	},
+
+	password: {
+		type: String,
+		required: [true, "Password is required"]
+	}
+
+},
+
+// schema constructor settings
+{
+	timestamps: {
+		createdAt: "dateAdded",
+		updatedAt: "dateUpdated"
+	}
+});
+
+const User = mongoose.model('User', userSchema);
+//							"User" -> "user" -> "users" collection name in the database
+
+module.exports = User;
